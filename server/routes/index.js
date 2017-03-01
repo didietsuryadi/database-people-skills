7825b8cff950adfe9e4f93aeea21869014c1c029
didietsuryadi <didietsuryadi>
2017-03-01 07:32:29 +0700

    create-controller-route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 const userController = require('../controllers/userController');
 
+//user route
 router.get('/', userController.getUsers);
 router.get('/:username', userController.getUser);
 router.post('/', userController.createUser);
 router.delete('/:username', userController.deleteUser);
 
+//user skill route
 router.get('/skills/:username', userController.getSkills);
 router.put('/addskill/:username', userController.addSkill);
 router.put('/removeskill/:username', userController.removeSkill);
